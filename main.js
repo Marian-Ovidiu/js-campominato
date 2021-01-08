6a41a02ff561137816523a85154f63e708a1749f
Marian Ovidiu Hutanu <h.marian914@gmail.com>
2021-01-08 16:42:30 +0100

    aggiornamento

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ var mine = [];
 var bool = false;
 var i = 0;
 
+function randomGenerator(){
+  return Math.floor(Math.random() * (100 + 1 - 1) + 1);
+}
+mina = randomGenerator();
+mine.push(mina)
+
 // costruisco array di mine e controllo che tutte abbiano un numoer diverso
-while (mine.length <= 15){
-    mina = Math.floor(Math.random() * (100 + 1 - 1) + 1);
+while (mine.length < 16){
+    mina = randomGenerator();
     console.log('Il numero generato è ' + mina);
 
     for(var j = 0; j <= mine.length; j++){
@@ -20,4 +26,24 @@ while (mine.length <= 15){
       mine.push(mina);
     }
 
+}
+console.log(mina);
+
+var bool1 = false;
+var imput;
+var x = 0;
+
+while (bool1 = false || x <= 84){
+  imput = prompt('Inserisci un numero fra 1 e 100');
+  for(var y = 0; y < mine.length; y++){
+    if(mine[y] === imput){
+      bool1 = true;
+    }
+  }
+  if(bool === false){
+    alert('Sei ancora salvo!!');
+  } else {
+    alert('Hai perso!!!');
+  }
+  x++
 }
